b4ed3f52bb9d7366a3eb123a3106cff7b04604ef
yohernes <134004269+yohernes@users.noreply.github.com>
2025-03-08 21:39:48 +0200

    not much

--- nrf_transmitter/nrf_transmitter.ino
+++ nrf_transmitter/nrf_transmitter.ino
@@ -2,29 +2,29 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-#define CE_PIN 9
-#define CSN_PIN 10
+RF24 radio(7, 8); // CE, CSN
 
-RF24 radio(CE_PIN, CSN_PIN);  // Create an RF24 object
-const byte address[6] = "00001";  // Address for communication
+const byte address[6] = "00001";
 
 void setup() {
-    Serial.begin(115200);
-    radio.begin();
-    radio.openWritingPipe(address);  // Set the address
-    radio.setPALevel(RF24_PA_LOW);   // Set power level
-    radio.stopListening();           // Set as transmitter
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  Serial.println("starting...");
+  if (radio.available())
+  {
+      Serial.println("radio available");
+
+  }
 }
 
 void loop() {
-    const char text[] = "hello";
-    bool sent = radio.write(&text, sizeof(text));  // Send message
-
-    if (sent) {
-        Serial.println("Message sent: hello");
-    } else {
-        Serial.println("Failed to send");
-    }
-
-    delay(500);  // Send every 500ms
+  const char text[] = "Hello world";
+  if(radio.write(&text, sizeof(text)))
+  {
+      Serial.println("sending");
+  };
+  delay(1000);
 }
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,31 +1,25 @@
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-#define CE_PIN 9
-#define CSN_PIN 10
+RF24 radio(7, 8); // CE, CSN
 
-RF24 radio(CE_PIN, CSN_PIN);
-const byte address[6] = "00001";  // Same address as transmitter
+const byte address[6] = "00001";
 
 void setup() {
-    Serial.begin(9600);
-    radio.begin();
-    delay(100);
-    Serial.println("setting up...");
-    radio.openReadingPipe(0, address);
-    radio.setDataRate(RF24_250KBPS);
-    radio.setPALevel(RF24_PA_HIGH);
-    radio.startListening(); // Set as receiver
-    
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
 void loop() {
-    if (radio.available()) {
-        char text[32] = "";
-        Serial.println("waiting...");
-        radio.read(&text, 5);
-        Serial.print("Received: ");
-        Serial.println(text);
-    }
+  if (radio.available()) {
+    Serial.println("waiting...");
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
 }
